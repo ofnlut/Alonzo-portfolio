0ab7a560455a1825f1f9f4ef26b42c1172a884ab
ofnlut <alonzo.mozee@gmail.com>
2024-02-13 04:42:00 -0500

    Stylized Video/VideoList components

--- components/Video.tsx
+++ components/Video.tsx
@@ -8,9 +8,15 @@ type VideoProps = {
 export const Video = ({title, link, thumbnail}: VideoProps) => {
     return (
         <div>
-            <a href={link}>
-            <img src={thumbnail}/>
-            <div>{title}</div>
+            <a className="relative w-full h-full bg-gray-900 group" href={link}>
+            <img className=" absolute inset-0 object-cover w-full h-full group-hover:opacity-50" src={thumbnail}/>
+            <div className="transition-all transform translate-y-8 opacity-0 group-hover:opacity-100 group-hover:translate-y-0">
+                <div className="relative p-4 pl-">
+                    <div className="mt-60 mb-5">
+                        {title}
+                    </div>
+                </div>
+            </div>
             </a>
             {/* <iframe width="840" height="472" src={link} title={title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
         </div>
--- components/VideoList.tsx
+++ components/VideoList.tsx
@@ -3,17 +3,17 @@ import { Video } from "./Video";
 
 // [title: Hello, youtubelink: link, image: image]
 const contentList = [
-    {title: "Versa Reign hoodie Ad", link: "https://www.youtube.com/embed/Fwr5sR-0l38?si=d71su-RSpZWift3-", thumbnail: "https://img.youtube.com/vi/Fwr5sR-0l38/hqdefault.jpg"},
+    {title: "Versa Reign hoodie Ad", link: "https://www.youtube.com/embed/Fwr5sR-0l38?si=d71su-RSpZWift3-", thumbnail: "https://img.youtube.com/vi/Fwr5sR-0l38/maxresdefault.jpg"},
     {title: "B.Pac - I Hope So", link: "https://www.youtube.com/embed/wGRfLtMUd7g?si=8fbS7YzYVsTqh4Rg", thumbnail: "https://img.youtube.com/vi/wGRfLtMUd7g/hqdefault.jpg"},
-    {title: "Chris Chase - Ben Simmons", link: "https://www.youtube.com/embed/J9rppUM-oug?si=j1dQywtgGZCKpzf4", thumbnail:"https://img.youtube.com/vi/J9rppUM-oug/hqdefault.jpg"},
-    {title: "FM Wu x Chris Chase - John Wick", link:"https://www.youtube.com/embed/bN4NIZda-Hw?si=gcvnHHFo0-JRfPUT", thumbnail:"https://img.youtube.com/vi/bN4NIZda-Hw/hqdefault.jpg"},
-    {title:"B.Pac - AHBLUGADA ", link:"https://www.youtube.com/embed/oS-2LKnxNm8?si=Fa-tw2jtAhHsCyZG", thumbnail:"https://img.youtube.com/vi/oS-2LKnxNm8/hqdefault.jpg"},
-    {title:"Mama's Boi - BERSERK", link:"https://www.youtube.com/embed/w-c7DdHJsF8?si=6_Bkhz1yXJONAF5e", thumbnail:"https://img.youtube.com/vi/w-c7DdHJsF8/hqdefault.jpg"},
-    {title:"Akiwele & Rakhi - Between Us", link:"https://www.youtube.com/embed/en7mreYGqQA?si=qn_IYa_hs3AS50Vp&amp;start=614", thumbnail:"https://img.youtube.com/vi/en7mreYGqQA/hqdefault.jpg"}
+    {title: "Chris Chase - Ben Simmons", link: "https://www.youtube.com/embed/J9rppUM-oug?si=j1dQywtgGZCKpzf4", thumbnail:"https://img.youtube.com/vi/J9rppUM-oug/maxresdefault.jpg"},
+    {title: "FM Wu x Chris Chase - John Wick", link:"https://www.youtube.com/embed/bN4NIZda-Hw?si=gcvnHHFo0-JRfPUT", thumbnail:"https://img.youtube.com/vi/bN4NIZda-Hw/maxresdefault.jpg"},
+    {title:"B.Pac - AHBLUGADA ", link:"https://www.youtube.com/embed/oS-2LKnxNm8?si=Fa-tw2jtAhHsCyZG", thumbnail:"https://img.youtube.com/vi/oS-2LKnxNm8/maxresdefault.jpg"},
+    {title:"Mama's Boi - BERSERK", link:"https://www.youtube.com/embed/w-c7DdHJsF8?si=6_Bkhz1yXJONAF5e", thumbnail:"https://img.youtube.com/vi/w-c7DdHJsF8/maxresdefault.jpg"},
+    {title:"Akiwele & Rakhi - Between Us", link:"https://www.youtube.com/embed/en7mreYGqQA?si=qn_IYa_hs3AS50Vp&amp;start=614", thumbnail:"https://img.youtube.com/vi/en7mreYGqQA/maxresdefault.jpg"}
 ]; 
 export const VideoList = () => {
     return (
-        <div className="grid grid-cols-4 gap-10">
+        <div className="grid grid-cols-2 gap-3">
             {contentList.map(content => ( 
                 <Video title={content.title} link={content.link} thumbnail={content.thumbnail} />
             ))}
