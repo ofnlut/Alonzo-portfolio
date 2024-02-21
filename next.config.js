7b38a1bcff38b2558c53fdb80488761e6584389a
ofnlut <alonzo.mozee@gmail.com>
2024-02-20 23:49:20 -0500

    Update basePath slug

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/nextjs-github-pages",
+  basePath: "/Alonzo-portfolio",
 
   /**
    * Disable server-based image optimization. Next.js does not support
